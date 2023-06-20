42365d6a612ae6fe455834704a69e64089adf285
BrianValdez <bbrriiaann1432@gmail.com>
2023-06-19 21:05:03 -0600

    mi primer commit

---
+++ src/main/java/com/mycompany/mavenproject/prueba/MavenprojectPrueba.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.mavenproject.prueba;
+
+/**
+ *
+ * @author brian
+ */
+public class MavenprojectPrueba {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+        System.out.println("prueba de git");
+    }
+}
